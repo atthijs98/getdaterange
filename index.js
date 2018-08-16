230fd0b8c1ef0e3b432c4cad059176cd1011bdfe
atthijs98 <matthijsvaneijk@gmail.com>
2018-08-16 10:18:24 +0200

    update index.js

--- index.js
+++ index.js
@@ -7,12 +7,14 @@ var endDate = date.toISOString().split('T')[0];
 
 var labels = getDateRange(startDate, endDate);
 
+/*array containing all the dates*/
+console.log(labels);
+
   function getDateRange(start,end) {
         var
             arr = [],
             dt = new Date(end);
-        console.log(dt);
-        console.log(start);
+    
         while (dt <= start) {
             arr.push(new Date(dt).toISOString().split("T")[0]);
             dt.setDate(dt.getDate() + 1);
